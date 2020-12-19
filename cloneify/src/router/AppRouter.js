53544b8ebfb0aeb6dd62d9f959e05fc1fb52880e
Greg Schuman <gcschuman@hotmail.com>
2020-12-19 14:00:12 -0800

    Added the root and loader to the index.html, added setExpiryTime declaration to Redirect.js, and added an expiryTime variable to the AppRouter.js.

--- cloneify/src/router/AppRouter.js
+++ cloneify/src/router/AppRouter.js
@@ -6,20 +6,40 @@ import Dashboard from '../components/Dashboard';
 import NotFound from '../components/NotFound';
 
 class AppRouter extends React.Component {
-    render() {
-      return (
-        <BrowserRouter>
-          <div className="main">
-            <Switch>
-              <Route path="/" component={Home} exact={true} />
-              <Route path="/redirect" component={Redirect} />
-              <Route path="/dashboard" component={Dashboard} />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </BrowserRouter>
-      );
+  state = {
+    expiryTime: '0'
+  };
+  componentDidMount() {
+    let expiryTime;
+    try {
+      expiryTime = JSON.parse(localStorage.getItem('expiry_time'));
+    } catch (error) {
+      expiryTime = '0';
     }
+    this.setState({ expiryTime });
   }
-
-export default AppRouter
+  setExpiryTime = (expiryTime) => {
+    this.setState({ expiryTime });
+  };
+  isValidSession = () => {
+    const currentTime = new Date().getTime();
+    const expiryTime = this.state.expiryTime;
+    const isSessionValid = currentTime < expiryTime;
+    return isSessionValid;
+  };
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="main">
+          <Switch>
+            <Route path="/" component={Home} exact={true} />
+            <Route path="/redirect" component={Redirect} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+export default AppRouter;
